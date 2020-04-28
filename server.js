3e0baddbdce5608b2bcdb1604be43ebef96a3f4b
Glitch (hello-express) <none>
2020-04-28 10:54:23 +0000

    🚴👻 Checkpoint ./server.js:15602780/41 ./config.json:15602780/118

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { TOKEN, PREFIX } = require("./config.json")
 //CLIENT EVENTS
 client.on("ready", () => {
   console.log('Şarkıları Çalmaya Hazırım')
-  client.user.setActivity(" Loz 'Bey | Müzik")
+  client.user.setActivity(" DJ ARMUT AKTIF ")
 })
 
 client.on("warn", info => console.log(info));
