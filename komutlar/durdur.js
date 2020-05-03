54438ca2f6ee45c6651933877355e8fccbf475ec
Glitch (dj-armut) <none>
2020-05-03 00:04:52 +0000

    📡🌃 Checkpoint ./config.json:3756271/137 ./komutlar/durdur.js:3756271/12377

--- komutlar/durdur.js
+++ komutlar/durdur.js
@@ -1,8 +1,31 @@
+const Discord = require("discord.js");
+
 module.exports = {
   name: "durdur",
   description: "şarkıyı duraklat",
   execute (client, message, args) {
-  const { channel } = message.member.voice;
+  const embed = new Discord.RichEmbed()
+    .setColor("BLACK")
+    .setDescription(
+      ` Müziği Durdurmayı Onaylıyormusunuz?`
+    )
+  .setFooter(client.user.username, client.user.avatarURL)
+  message.channel.send(embed).then(async function(sentEmbed) {
+    const emojiArray = ["✅"];
+    const filter = (reaction, user) =>
+      emojiArray.includes(reaction.emoji.name) && user.id === message.author.id;
+    await sentEmbed.react(emojiArray[0]).catch(function() {});
+    var reactions = sentEmbed.createReactionCollector(filter, {
+      time: 30000
+    });
+    reactions.on("end", () => sentEmbed.edit("İşlem iptal oldu!"));
+    reactions.on("collect", async function(reaction) {
+      if (reaction.emoji.name === "✅") {
+        message.channel.send(
+          `İşlem onaylandı! Müzik Durduruldu.`
+        );
+
+          const { channel } = message.member.voice;
     if (!channel) {
       //IF AUTHOR IS NOT IN VOICE CHANNEL
       return message.channel.send("SES KANALINDA OLMALISINIZ: /");
@@ -20,6 +43,9 @@ module.exports = {
       
       
       return message.channel.send("✅ | Çalmakta Olan Şarkıyı Duraklattı")
-  }  
+  }
+      }
+    });
+  }); 
   }
 }
