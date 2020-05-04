64317271665468534e41fe804397219f3f3005ca
Glitch (dj-armut) <none>
2020-05-04 04:45:12 +0000

    Before import of YnsEmreArpacii/muzikbotu without specified path

--- komutlar/ping.js
+++
@@ -1,9 +0,0 @@
-//FIRST TEST HANDLER IS WORKING OR NOT
-module.exports = {
-  name: "ping",
-  description: "Botun pinglenmesi",
-  execute(client, message) {
-  message.channel.send('PONG :)')
-    
-}
-}
